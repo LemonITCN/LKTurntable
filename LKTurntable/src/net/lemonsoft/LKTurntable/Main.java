4cfccf64b2f706337fa3caa2f5cfb063225f4ed3
LiuRi <liuri@lemonsoft.net>
2016-10-15 10:03:39 +0800

    init project

--- LKTurntable/src/net/lemonsoft/LKTurntable/Main.java
+++ LKTurntable/src/net/lemonsoft/LKTurntable/Main.java
@@ -10,9 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("view/ui-main.fxml"));
+        root.getStylesheets().add(getClass().getResource("view/ui-main.css").toExternalForm());
+        primaryStage.setTitle("LKTurntable");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
---
+++ LKTurntable/src/net/lemonsoft/LKTurntable/logic/PrizeManager.java
@@ -0,0 +1,9 @@
+package net.lemonsoft.LKTurntable.logic;
+
+/**
+ * Created by lemonsoft on 16-10-14.
+ */
+public class PrizeManager {
+
+
+}
